b773b77b372869f8272e4a77e2053e360aa1bb97
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2024-08-23 10:34:29 +0000

    feat: row_with_separator

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: gen apk ncommit dep
 
-VERSION = 0.0.21
+VERSION = 0.0.22
 
 gen:
 	flutter pub run build_runner build --delete-conflicting-outputs
--- commons/lib/ui/widgets/widgets.dart
+++ commons/lib/ui/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export "busy_indicator/busy_indicator.dart";
 export "button/button.dart";
+export "row_with_separator/row_with_separator.dart";
 export "space/space.dart";
